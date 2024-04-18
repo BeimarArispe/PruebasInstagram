b023d16512e4da84db95588d984b15ffc4e03e59
BeimarArispe <201900388@est.umss.edu>
2024-04-18 12:12:50 -0400

    prueba datos

---
+++ defines.php
@@ -0,0 +1,4 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '1368132997230381' );
+	define( 'INSTAGRAM_APP_SECRET', 'd83873a62a906dfce475e84130ad1632' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://65ff82f4eb203775071fafdd--prismatic-horse-124283.netlify.app/' );
