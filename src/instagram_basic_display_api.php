5238a24c34fd2794cd9a960879c11d5c9b2ba241
BeimarArispe <201900388@est.umss.edu>
2024-04-18 23:04:47 -0400

    prueba datos8

--- src/instagram_basic_display_api.php
+++ src/instagram_basic_display_api.php
@@ -7,13 +7,11 @@
 		private $_redirectUrl = INSTAGRAM_APP_REDIRECT_URI;
 		private $_getCode = '';
 		private $_apiBaseUrl = 'https://api.instagram.com/';
-		private $_graphBaseUrl = 'https://graph.instagram.com/';
 		private $_userAccessToken = '';
-		private $_userAccessTokenExpires = '';
 
-		public $authorizationUrl = '';
 		public $hasUserAccessToken = false;
-		public $userId = '';
+		public $authorizationUrl = '';
+	
 
 		function __construct( $params ) {
 			// save instagram code
@@ -30,10 +28,6 @@
 			return $this->_userAccessToken;
 		}
 
-		public function getUserAccessTokenExpires() {
-			return $this->_userAccessTokenExpires;
-		}
-
 		private function _setAuthorizationUrl() {
 			$getVars = array( 
 				'app_id' => $this->_appId,
@@ -47,20 +41,10 @@
 		}
 
 		private function _setUserInstagramAccessToken( $params ) {
-			if ( $params['access_token'] ) { // we have an access token
-				$this->_userAccessToken = $params['access_token'];
-				$this->hasUserAccessToken = true;
-				$this->userId = $params['user_id'];
-			} elseif ( $params['get_code'] ) { // try and get an access token
+			if ( $params['get_code'] ) { // we have an access token
 				$userAccessTokenResponse = $this->_getUserAccessToken();
 				$this->_userAccessToken = $userAccessTokenResponse['access_token'];
 				$this->hasUserAccessToken = true;
-				$this->userId = $userAccessTokenResponse['user_id'];
-
-				// get long lived access token
-				$longLivedAccessTokenResponse = $this->_getLongLivedUserAccessToken();
-				$this->_userAccessToken = $longLivedAccessTokenResponse['access_token'];
-				$this->_userAccessTokenExpires = $longLivedAccessTokenResponse['expires_in'];
 			}
 		}
 
@@ -81,85 +65,6 @@
 			return $response;
 		}
 
-		private function _getLongLivedUserAccessToken() {
-			$params = array(
-				'endpoint_url' => $this->_graphBaseUrl . 'access_token',
-				'type' => 'GET',
-				'url_params' => array(
-					'client_secret' => $this->_appSecret,
-					'grant_type' => 'ig_exchange_token',
-				)
-			);
-
-			$response = $this->_makeApiCall( $params );
-			return $response;
-		}
-
-		public function getUser() {
-			$params = array(
-				'endpoint_url' => $this->_graphBaseUrl . 'me',
-				'type' => 'GET',
-				'url_params' => array(
-					'fields' => 'id,username,media_count,account_type',
-				)
-			);
-
-			$response = $this->_makeApiCall( $params );
-			return $response;
-		}
-
-		public function getUsersMedia() {
-			$params = array(
-				'endpoint_url' => $this->_graphBaseUrl . $this->userId . '/media',
-				'type' => 'GET',
-				'url_params' => array(
-					'fields' => 'id,caption,media_type,media_url',
-				)
-			);
-
-			$response = $this->_makeApiCall( $params );
-			return $response;
-		}
-
-		public function getPaging( $pagingEndpoint ) {
-			$params = array(
-				'endpoint_url' => $pagingEndpoint,
-				'type' => 'GET',
-				'url_params' => array(
-					'paging' => true
-				)
-			);
-
-			$response = $this->_makeApiCall( $params );
-			return $response;
-		}
-
-		public function getMedia( $mediaId ) {
-			$params = array(
-				'endpoint_url' => $this->_graphBaseUrl . $mediaId,
-				'type' => 'GET',
-				'url_params' => array(
-					'fields' => 'id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username'
-				)
-			);
-
-			$response = $this->_makeApiCall( $params );
-			return $response;
-		}
-
-		public function getMediaChildren( $mediaId ) {
-			$params = array(
-				'endpoint_url' => $this->_graphBaseUrl . $mediaId . '/children',
-				'type' => 'GET',
-				'url_params' => array(
-					'fields' => 'id,media_type,media_url,permalink,thumbnail_url,timestamp,username'
-				)
-			);
-
-			$response = $this->_makeApiCall( $params );
-			return $response;
-		}
-
 		private function _makeApiCall( $params ) {
 			$ch = curl_init();
 
@@ -168,13 +73,7 @@
 			if ( 'POST' == $params['type'] ) { // post request
 				curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $params['url_params'] ) );
 				curl_setopt( $ch, CURLOPT_POST, 1 );
-			} elseif ( 'GET' == $params['type'] && !$params['url_params']['paging'] ) { // get request
-				$params['url_params']['access_token'] = $this->_userAccessToken;
-
-				//add params to endpoint
-				$endpoint .= '?' . http_build_query( $params['url_params'] );
 			}
-
 			// general curl options
 			curl_setopt( $ch, CURLOPT_URL, $endpoint );
 
